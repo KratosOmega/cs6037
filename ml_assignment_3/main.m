ff460355109a0e15dd31204c0e64beded4fc4414
Kratos <Kratos.X.Omega@gmail.com>
2018-11-26 01:16:59 -0500

    add iris dataset for consistence

--- ml_assignment_3/main.m
+++ ml_assignment_3/main.m
@@ -7,20 +7,32 @@
 %######################################################################################
 
 
-
 close all; clear all; clc;
 
-[training_data_set, testing_data_set, data] = makeData();
+%[training_data_set, testing_data_set, data] = makeData();
+
+data=csvread('iris_data.csv');
+training_data_set=csvread('iris_training_data_set.csv');
+testing_data_set=csvread('iris_testing_data_set.csv');
+
+data_size = size(data, 1)
 
 [w, b, a] = SMO(data, 0.1, 0.1, 0.5, training_data_set);
 
 
-% Accuracy and F-measure
-x=testing_data_set(:,1:end-1);
-y=testing_data_set(:,end);
+%----------------------------------------------------- plot training
+x=training_data_set(:,1:end-1);
+y=training_data_set(:,end);
 
-fx=sign(w*x'+b)';
-[~, Accuracy, F_measure] = summary(y, fx)
+
+
+
+
+% Weights
+w=sum(a.*y*x)
+% Bias
+b =mean(y - x*w')
+
 
 %Plotting the Decision Boundry
 hold on
@@ -28,21 +40,42 @@ scatter(x(y==1,1),x(y==1,2),'b')
 scatter(x(y==-1,1),x(y==-1,2),'r')
 
 syms x1 x2
+
 fn=vpa((w(1)*x1+b)/w(2),6);
 fn1=vpa((w(1)*x1+b-1)/w(2),6);
 fn2=vpa((w(1)*x1+b+1)/w(2),6);
+
+% fn=vpa((-b-w(1)*x1)/w(2),6);
+% fn1=vpa((-1-b-w(1)*x1)/w(2),6);
+% fn2=vpa((1-b-w(1)*x1)/w(2),6);
+
+
 fplot(fn,'Linewidth',2);
 fplot(fn1,'Linewidth',1);
 fplot(fn2,'Linewidth',1);
-axis([2 10 1 5])
+%axis([2 10 1 5])
+%axis([-10 10 -10 10])
+axis([-20 20 -20 20])
 xlabel ('Positive Class: blue, Negative Class: red')
 hold off
+
+%----------------------------------------------------- test summary
+
+% Accuracy and F-measure
+xtest=testing_data_set(:,1:end-1);
+ytest=testing_data_set(:,end);
+
+fx=sign(w*xtest'+b)';
+[~, Accuracy, F_measure] = summary(ytest, fx)
+
+
+
 
 
 
 
 %######################################################################################
-% 
+
 % 
 % close all; clear all; clc;
 % 
@@ -61,27 +94,29 @@ hold off
 % % testing_data_set = data(seq((data_size/2)+1:end),:);
 % 
 % 
+% ratio = 0.8;
 % 
 % % Set training data set
-% training_data_set = data(1:(data_size*0.8),:);
+% training_data_set = data(1:(data_size*ratio),:);
 % 
 % % Set testing data set
-% testing_data_set = data((data_size*0.8):end,:);
-% 
-% 
-% 
-% 
+% testing_data_set = data((data_size*ratio):end,:);
 % 
 % [w, b, a] = SMO(data, 0.1, 0.1, 0.5, training_data_set);
 % 
-% % Accuracy and F-measure
-% x=testing_data_set(:,1:end-1);
-% y=testing_data_set(:,end);
 % 
-% fx=sign(w*x'+b)';
-% [~, Accuracy, F_measure] = summary(y, fx)
+% 
+% 
+% 
 % 
 % %Plotting the Decision Boundry
+% x=training_data_set(:,1:end-1);
+% y=training_data_set(:,end);
+% 
+% 
+% 
+% 
+% 
 % hold on
 % scatter(x(y==1,1),x(y==1,2),'b')
 % scatter(x(y==-1,1),x(y==-1,2),'r')
@@ -103,6 +138,22 @@ hold off
 % axis([-2 2 -2 2])
 % xlabel ('Positive Class: blue, Negative Class: red')
 % hold off
+% 
+% % %----------------------------------------------------- test summary
+% 
+% % Accuracy and F-measure
+% x_test=testing_data_set(:,1:end-1);
+% y_test=testing_data_set(:,end);
+% 
+% fx=sign(w*x_test'+b)';
+% [~, Accuracy, F_measure] = summary(y_test, fx)
+% 
+% 
+
+
+
+
+
 
 
 
--- ml_assignment_3/makeData.m
+++ ml_assignment_3/makeData.m
@@ -17,19 +17,28 @@ for i = 51:150
     data(i,5) = -1;
 end
 
+% set train/test ratio
+subRatio = 0.8;
+
 % Split the data set into 2 subsets
 seq = randperm(numel(linspace(1, data_size, data_size)));
 
+
 % Set training data set
-training_data_set = data(seq(1:(data_size/2)),:);
+training_data_set = data(seq(1:(data_size*subRatio)),:);
 
 % Set testing data set
-testing_data_set = data(seq((data_size/2)+1:end),:);
+testing_data_set = data(seq((data_size*subRatio)+1:end),:);
+
 
 % Col 1: Sepal Length
 % Col 2: Sepal Width
 % Col 3: Petal Length
 % Col 4: Petal Width
 % Col 5: Label (1: Setosa, -1: not Setosa)
+
+csvwrite('iris_data.csv',data)
+csvwrite('iris_training_data_set.csv',training_data_set)
+csvwrite('iris_testing_data_set.csv',testing_data_set)
 
 
